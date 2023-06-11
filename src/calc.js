03e6b29dd7892e88b33c846b8919259bd7406fe9
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-11 16:54:51 +0530

    export function on itself.

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,13 @@
 // by default every member is private.
-function sum(n1, n2) {
+export function sum(n1, n2) {
     return n1 + n2;
   }
   
+  export function subtraction(n1, n2) {
+    return n1 - n2;
+  }
+  
   // you have to make it export (public) manually
-  export { sum };
+  // export { sum };
+  // export { subtraction };
+  // export { sum, subtraction };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { sum } from "./calc.js";
+// import { sum } from "./calc.js";
+// import { subtraction } from "./calc.js";
+
+import { sum, subtraction } from "./calc.js";
 
 let output = sum(10, 20);
 console.log("Sum=" + output);
+
+let output1 = subtraction(50, 10);
+console.log("Diff=" + output1);
