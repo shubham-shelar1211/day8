471dafd47ef8ffcfe3ea9dc7cd31d903f473a81a
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-11 16:48:41 +0530

    basic module

---
+++ src/calc.js
@@ -0,0 +1,7 @@
+// by default every member is private.
+function sum(n1, n2) {
+    return n1 + n2;
+  }
+  
+  // you have to make it export (public) manually
+  export { sum };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+import { sum } from "./calc.js";
+
+let output = sum(10, 20);
+console.log("Sum=" + output);
